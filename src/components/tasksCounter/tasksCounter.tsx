da7186c6b0dc98ef4d6ffb0aca85bb6812bbb707
ricardobar96 <ricardobar96@gmail.com>
2025-10-10 23:55:58 +0100

    Added tasksCounter component

---
+++ src/components/tasksCounter/tasksCounter.tsx
@@ -0,0 +1,12 @@
+export function TasksCounter() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="p-6 border-solid border-yellow-500 border-4 rounded-full mb-4">
+        <div className="min-w-10 min-h-10 text-white text-3xl text-center flex justify-center leading-10">
+          12
+        </div>
+      </div>
+      <div className="text-white text-xl text-center">Todo</div>
+    </div>
+  );
+}
