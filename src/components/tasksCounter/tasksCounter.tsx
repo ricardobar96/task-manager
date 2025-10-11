cfd263be452378fef2151eb007cc49b60bad6972
ricardobar96 <ricardobar96@gmail.com>
2025-10-11 17:19:07 +0100

    Set task's color based on its status

--- src/components/tasksCounter/tasksCounter.tsx
+++ src/components/tasksCounter/tasksCounter.tsx
@@ -5,7 +5,10 @@ export const TasksCounter: FC<ITaskCounter> = (props): ReactElement => {
   const {status, count} = props;
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className={`p-6 border-solid border-yellow-500 border-4 rounded-full mb-4`}>
+      <div className={`p-6 border-solid border-4 rounded-full mb-4 
+        ${status === "todo" && "border-red-500"}
+        ${status === "inProgress" && "border-yellow-500"}
+        ${status === "completed" && "border-green-500"}`}>
         <div className="min-w-10 min-h-10 text-white text-3xl text-center flex justify-center leading-10">
           {count}
         </div>
