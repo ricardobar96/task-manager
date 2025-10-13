2ea6dd60484a9bcb5d135274534b84933a372202
ricardobar96 <ricardobar96@gmail.com>
2025-10-13 19:23:39 +0100

    User profile component

--- src/components/taskSidebar/taskSidebar.tsx
+++ src/components/taskSidebar/taskSidebar.tsx
@@ -1,13 +1,14 @@
 import type { FC, ReactElement } from "react";
 import { Card } from "@/components/ui/card";
+import styles from "./style.module.css";
 import { UserProfile } from "../userProfile/userProfile";
 import { CreateTaskForm } from "../createTaskForm/createTaskForm";
 import { Logout } from "../logout/logout";
 
 export const TaskSidebar: FC = (): ReactElement => {
   return (
-    <section>
-      <Card>
+    <section className={`fixed right-4 top-4 ${styles.sidebarHeight}`}>
+      <Card className="flex flex-col w-full h-full p-6 justify-between">
         <UserProfile />
         <CreateTaskForm />
         <Logout />
--- src/components/userProfile/userProfile.tsx
+++ src/components/userProfile/userProfile.tsx
@@ -1,3 +1,23 @@
-export const UserProfile = () => {
-  return <div>User</div>;
+import type { FC, ReactElement } from "react";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+
+export const UserProfile: FC<{ firstName: string, lastName: string }> = (props): ReactElement => {
+  const { firstName = "Ricardo", lastName = "Baloira" } = props;
+
+  return (
+    <div className="flex flex-col w-full py-8 items-center">
+      <Avatar className={`mb-4 ${cn("h-24", "w-24")}`}>
+        <AvatarFallback
+          className={`text-2xl font-semibold ${cn(
+            "bg-sky-600",
+            "dark:bg-sky-600"
+          )}`}
+        >
+          {firstName.slice(0, 1)}{lastName.slice(0, 1)}
+        </AvatarFallback>
+      </Avatar>
+      <h4>{firstName} {lastName}</h4>
+    </div>
+  );
 };
