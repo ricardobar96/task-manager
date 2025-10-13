35bb16c76a49bccf2f18b5e82ec498f25ae016d8
ricardobar96 <ricardobar96@gmail.com>
2025-10-13 19:32:10 +0100

    Form layout for tasks creation

--- src/components/createTaskForm/createTaskForm.tsx
+++ src/components/createTaskForm/createTaskForm.tsx
@@ -1,3 +1,14 @@
+import { Input } from "@/components/ui/input";
+
 export const CreateTaskForm = () => {
-  return <div>New Task</div>;
+   return (
+    <div>
+      <h2 className="text-xl mb-4">Create task</h2>
+      <form>
+        <div className="py-2">
+          <Input type="text" placeholder="Title" />
+        </div>
+      </form>
+    </div>
+   );
 };
--- src/components/taskSidebar/taskSidebar.tsx
+++ src/components/taskSidebar/taskSidebar.tsx
@@ -9,7 +9,7 @@ export const TaskSidebar: FC = (): ReactElement => {
   return (
     <section className={`fixed right-4 top-4 ${styles.sidebarHeight}`}>
       <Card className="flex flex-col w-full h-full p-6 justify-between">
-        <UserProfile />
+        <UserProfile firstName="Ricardo" lastName="Baloira"/>
         <CreateTaskForm />
         <Logout />
       </Card>
